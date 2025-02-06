5624088dbfc861ef2bf908009537ef095e26ab15
HaolinZhong-humber <n01617780@students.humber.ca>
2025-02-05 23:19:10 -0500

    Fixed errors in posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,60 +1,40 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 
-const JsonApi = () => {
-
-const [posts, setPosts] = useState([]);
-
-useEffect(() => {
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-
-.then((response) => response.json())
-
-.then((responseData) => {
-
-setPosts(responseData);
-
-console.log(responseData);
-
-});
-
-
-}, []);
-
-
-return (
-
-<div>
-
-{posts.map((post) => (
-
-<Post post={post} key={post.id} />
-
-))}
-
-</div>
-
-);
-
+type PostType = {
+  id: number;
+  title: string;
+  body: string;
 };
 
+const JsonApi = () => {
+  const [posts, setPosts] = useState<PostType[]>([]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseData: PostType[]) => {
+        setPosts(responseData);
+        console.log(responseData);
+      });
+  }, []);
 
-const Post = (props) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
 
-return (
-
-<div>
-
-<h1>{props.post.title}</h1>
-
-<p>{props.post.body}</p>
-
-</div>
-
-);
-
+const Post = ({ post }: { post: PostType }) => {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default JsonApi;
